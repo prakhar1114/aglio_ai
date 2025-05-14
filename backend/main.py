d1465c5c3f71315e8c12c16c582a853f694e583c
Prakhar Jain <prakharjain1114@gmail.com>
2025-05-14 18:36:06 +0530

    removed autoreload

--- backend/main.py
+++ backend/main.py
@@ -141,4 +141,9 @@ def history(session_id: str):
     }
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8005, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8005,
+        # reload=True
+    )
